9d097ce198c942bb7e79460c2946415f25fc5bfa
broadcast80 <134933966+broadcast80@users.noreply.github.com>
2024-04-14 19:51:14 +0300

    Add files via upload

---
+++ db/migrations/001_name_of_migration.sql
@@ -0,0 +1,89 @@
+-- Write your migrate up statements here
+CREATE TYPE genders AS ENUM (
+	'male',
+	'female');
+
+CREATE TABLE IF NOT EXISTS person (
+  id INT generated always as IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  birthday DATE NOT NULL DEFAULT CURRENT_DATE,
+  description TEXT NOT NULL,
+  location TEXT NOT NULL, 
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  premium BOOLEAN NOT NULL DEFAULT FALSE,
+  likes_left INTEGER NOT NULL DEFAULT '10',
+  gender genders NOT NULL
+);
+
+create table if not exists interest (
+  id int generated always as IDENTITY PRIMARY KEY,
+  name text not null
+);
+
+create table if not exists person_interest (
+  person_id int not null references person(id)
+    on delete cascade
+    on update cascade,
+  interest_id int not null references interest(id)
+    on delete cascade
+    on update cascade,
+    
+  primary key(person_id, interest_id)
+);
+
+create table if not exists "like" (
+  person_one_id int not null references person(id)
+    on delete cascade
+    on update cascade,
+  person_two_id int not null references person(id)
+    on delete cascade
+    on update cascade,
+  
+  primary key(person_one_id, person_two_id)
+);
+
+create table if not exists dislike (
+  person_one_id int not null references person(id)
+    on delete cascade
+    on update cascade,
+  person_two_id int not null references person(id)
+    on delete cascade
+    on update cascade,
+  
+  primary key(person_one_id, person_two_id)
+);
+
+CREATE TABLE IF NOT EXISTS person_image (
+    person_id int generated always as IDENTITY REFERENCES person(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    image_url text NOT NULL,
+    cell_number int not null DEFAULT '1',
+    PRIMARY KEY (person_id, cell_number)
+);
+
+CREATE TABLE IF NOT EXISTS person_premium (
+	id int generated always as IDENTITY PRIMARY KEY,
+	person_id int not null references person(id)
+    		on delete cascade
+    		on update cascade,
+	date_started date not null,
+	date_ended date
+);
+---- create above / drop below ----
+drop table person;
+
+drop table interest;
+
+drop table person_interest;
+
+drop table "like";
+
+drop table dislike;
+
+drop table person_image;
+
+drop table person_premium;
+
+-- Write your migrate down statements here. If this migration is irreversible
+-- Then delete the separator line above.
