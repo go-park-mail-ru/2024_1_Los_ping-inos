f2ad16224a4a24edc47ff82ccafcb53fe6155431
DeDeimos <rodion.tolkachev.03@inbox.ru>
2024-05-01 16:26:54 +0000

    working new domens

--- internal/pkg/responces.go
+++ internal/pkg/responces.go
@@ -23,7 +23,7 @@ func SendResponse(w http.ResponseWriter, r *http.Request, code int, Body any) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "https://jimder.ru")
+	w.Header().Set("Access-Control-Allow-Origin", "https://jimder.ru/")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Csrft")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
