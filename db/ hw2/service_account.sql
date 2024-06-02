f5649daacf38d27baaf900b42a0a89f7e5f77a3e
broadcast80 <134933966+broadcast80@users.noreply.github.com>
2024-06-02 13:03:20 +0300

    Update service_account.sql

--- db/ hw2/service_account.sql
+++ db/ hw2/service_account.sql
@@ -5,7 +5,6 @@ CREATE ROLE jimder_service_account_role WITH LOGIN;
 GRANT USAGE ON SCHEMA public TO jimder_service_account_role;
 
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO jimder_service_account_role;
-GRANT INSERT ON person, interest, person_interest, "like", dislike, 
-person_image, person_premium, "message", person_claim, 
-claim, person_payment TO jimder_service_account_role;
-GRANT DELETE ON 
+GRANT INSERT ON person_interest, person_payment, person, message, person_claim, "like", person_image TO jimder_service_account_role;
+GRANT DELETE ON person_interest, person, person_image TO jimder_service_account_role;
+GRANT UPDATE ON person TO jimder_service_account_role;
